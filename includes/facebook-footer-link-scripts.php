31d9a96bf390a7da6e69497b4d2435a571c4b60f
Zoran <zoran.mihelcic@gmail.com>
2016-01-27 13:43:39 +0000

    added hook to include function in wordpress

--- includes/facebook-footer-link-scripts.php
+++ includes/facebook-footer-link-scripts.php
@@ -2,5 +2,8 @@
 
 // Add scripts
 function ffl_add_scripts(){
-    
+    wp_enqueue_style('ffl-main-style', plugins_url().'/facebook-footer-link/css/style.css');
+    wp_enqueue_script('ffl-main-script', plugins_url().'/facebook-footer-link/js/main.js');
 }
+
+add_action('wp_enqueue_scripts','ffl-add-scripts');
