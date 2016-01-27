f79db33867aa6363e47e5cd088b10bbada2b2406
Zoran <zoran.mihelcic@gmail.com>
2016-01-27 13:14:44 +0000

    added basic plugin description in script

---
+++ facebook-footer-link.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Facebook Footer Link
+ * Description: Adds a Facebook profile link to the end of posts
+ * Version: 1.0
+ * Author: Zoran Mihelcic
+ * 
+ **/
+ //Exit if Accessed Diretctliy
+ if(!defined('ABSPATH')){
+     exit;
+ }
