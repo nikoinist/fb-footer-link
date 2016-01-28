fd973744912b10b65ef33b4733ddff9abe078b74
Zoran <zoran.mihelcic@gmail.com>
2016-01-28 18:08:12 +0000

    added settings options to work

--- includes/facebook-footer-link-content.php
+++ includes/facebook-footer-link-content.php
@@ -1,12 +1,28 @@
 <?php
 
 function ffl_add_footer($content){
+    
+    global $ffl_options; 
+    
     $footer_output = '<hr>';
     $footer_output .= '<div class="footer_content">';
-    $footer_output .= '<span class="dashicons dashicons-facebook"></span>Find me on <a href="http://www.facebook.com" target="_blank">Facebook</a>';
+    $footer_output .= '<span class="dashicons dashicons-facebook"></span>Find me on <a style="color:'.$ffl_options[link_color].'" href="'.$ffl_options[facebook_url].'" target="_blank">Facebook</a>';
     $footer_output .= '</div>';
     
-    return $content . $footer_output;
+    if($ffl_options['show_in_feed']){
+        
+            //Show only in page not feed
+        if(is_single() || is_home() && $ffl_options['enable']){
+            return $content . $footer_output;
+        } 
+    } else {
+        if(is_single() && $ffl_options['enable']){
+            return $content . $footer_output;
+        } 
+    }
+    
+
+    return $content;
 }
 
 add_filter('the_content', 'ffl_add_footer');
